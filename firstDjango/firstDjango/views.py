601c304601863fc20501f377a929e226f54f8786
arya <aryapawar92@gmail.com>
2024-11-03 18:57:07 +0530

    templates

---
+++ firstDjango/firstDjango/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    #return HttpResponse("Hello, world! home")
+    return render(request , 'website/index.html')
+
+def about(request):
+    return HttpResponse("Hello, world! about")
+
+def contact(request):
+    return HttpResponse("Hello, world! contact")
