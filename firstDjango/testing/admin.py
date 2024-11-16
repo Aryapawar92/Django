96540c97c3520fc5811514698357b64c44d1d3f0
arya <aryapawar92@gmail.com>
2024-11-16 16:19:43 +0530

    last django

--- firstDjango/testing/admin.py
+++ firstDjango/testing/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from .models import ChaiVarity
+from .models import ChaiVarity , chaiReview , store , certified
 
 # Register your models here.
 
-admin.site.register(ChaiVarity)
+class chaiReviewInLine(admin.TabularInline):
+    model = chaiReview
+    extra = 2
+
+class chaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name' , 'type' , 'date_added')
+    inlines = [chaiReviewInLine]
+
+class storeAdmin(admin.ModelAdmin):
+    list_display = ('name' , 'location')
+    filter_horizontal = ('chai_varieties' ,)
+
+class chaicertificate(admin.ModelAdmin):
+    list_display = ('chai' , 'certificate_number')
+
+admin.site.register(ChaiVarity , chaiVarietyAdmin)
+admin.site.register(store)
+admin.site.register(certified , chaicertificate) 
+
+
+
+
