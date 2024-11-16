bbd941e9c0e0d5c35a5ab9be209e38f993ebd582
arya <aryapawar92@gmail.com>
2024-11-16 13:57:06 +0530

    handle model

--- firstDjango/testing/models.py
+++ firstDjango/testing/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class ChaiVarity(models.Model):
+    type_choice = [('ML','MASALA'),('GR','GINGER'),('KL','KIWI'),('PL','PLAIN')]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chais/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2,choices=type_choice)
+    def __str__(self):
+        return self.name 
+
+
--- firstDjango/testing/views.py
+++ firstDjango/testing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import ChaiVarity
 
 # Create your views here.
 
 def all_test(request):
-    return render(request , 'tester/all_test.html')
+    chais = ChaiVarity.objects.all()
+    return render(request , 'tester/all_test.html',{'chais' : chais})
